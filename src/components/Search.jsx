b04c94b34ca7d9d6add64b43bab9bb97e4edb104
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2023-12-19 01:33:30 +0530

    initialized a search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
-  return (
-    <section className="py-4 container">
-        <div className='flex justify-center'>
-            <div className='w-full md:w-1/2 lg:w-1/4 mx-0 mb-4'>
-                <div className='bg-white rounded-lg shadow'>
-                    <img src='' className='w-full' alt='searchCards'/>
-                    <div className='p-4'>
-                        <h5 className='text-xl font-bold'>card title</h5>
-                        <p className='text-gray-600'>card desc</p>
+    const [data, setData] = useState([]);
+    const [filterData, setfilterData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.spacexdata.com/v3/rockets")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.map(({ rocket_name }) => rocket_name)); // Display rocket_name in console
+                const filteredData = data.map(({ active, rocket_name, description }) => ({ rocket_name, active, description }));
+                setfilterData(filteredData);
+                setData(filteredData); // Update the data state
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const handleFilter = (value) => {
+        const res = filterData.filter(f => f.rocket_name.toLowerCase().includes(value))
+        setData(res);
+        if(value === ""){
+            setData([]);
+        }
+    }
+
+    return (
+        <div className='search-top w-30'>
+            <div className='search w-100 m-10'>
+                <input type="text" placeholder="Search Here...." onChange={e => handleFilter(e.target.value)} className="w-full px-6 py-4 text-xl" />
+            </div>
+            <div className='search-result w-100   bg-white'>
+                {data.map((d, i)=> (
+                    <div key={i}>
+                        <h3>{d.rocket_name}</h3>
+                        <p>Active: {d.active ? 'Yes' : 'No'}</p>
+                        <p>{d.description}</p>
+                        
                     </div>
-                </div>
+                ))}
             </div>
         </div>
-    </section>
-  )
-}
+    );
+};
 
-export default Search
+export default Search;
